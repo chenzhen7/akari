3bfad2ac1bbe79db2c2c0f1c7ace680620ec65db
chenzhen <1583296383@qq.com>
2026-06-13 20:46:35 +0800

    右侧边栏四个面板现在同时挂载，通过 hidden 控制显隐，切换 tab 时不再重新加载文件树 / Diff 列表。

--- apps/web/src/components/layout/RightSidebar.tsx
+++ apps/web/src/components/layout/RightSidebar.tsx
@@ -79,33 +79,25 @@ export function RightSidebar({ session }: RightSidebarProps) {
         ))}
       </div>
 
-      {/* Content */}
+      {/* Content: keep-alive with display:none so panels don't remount on tab switch */}
       <div className="relative flex-1 overflow-hidden">
         {session ? (
           <>
-            {activeRightTab === 'explorer' && (
-              <div className="absolute inset-0 overflow-hidden">
-                <ExplorerPanel session={session} onOpenFile={handleOpenFile} />
-              </div>
-            )}
-            {activeRightTab === 'diff' && (
-              <div className="absolute inset-0 overflow-hidden">
-                <DiffFileList
-                  session={session}
-                  onSelectFile={handleSelectFile}
-                />
-              </div>
-            )}
-            {activeRightTab === 'git-graph' && (
-              <div className="absolute inset-0">
-                <GitGraphPanel sessionId={session.id} />
-              </div>
-            )}
-            {activeRightTab === 'info' && (
-              <div className="absolute inset-0 overflow-hidden">
-                <SessionInfoPanel session={session} />
-              </div>
-            )}
+            <div className={cn('absolute inset-0 overflow-hidden', activeRightTab !== 'explorer' && 'hidden')}>
+              <ExplorerPanel session={session} onOpenFile={handleOpenFile} />
+            </div>
+            <div className={cn('absolute inset-0 overflow-hidden', activeRightTab !== 'diff' && 'hidden')}>
+              <DiffFileList
+                session={session}
+                onSelectFile={handleSelectFile}
+              />
+            </div>
+            <div className={cn('absolute inset-0', activeRightTab !== 'git-graph' && 'hidden')}>
+              <GitGraphPanel sessionId={session.id} />
+            </div>
+            <div className={cn('absolute inset-0 overflow-hidden', activeRightTab !== 'info' && 'hidden')}>
+              <SessionInfoPanel session={session} />
+            </div>
           </>
         ) : (
           <div className="flex h-full items-center justify-center text-muted-foreground">
--- apps/web/src/components/layout/SessionSidebar.tsx
+++ apps/web/src/components/layout/SessionSidebar.tsx
@@ -183,7 +183,7 @@ const SessionItem = memo(function SessionItem({
       )}
     </>
   )
-}
+})
 
 export function SessionSidebar() {
   const sessions = useSessionStore(s => s.sessions)
